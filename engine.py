a432440c3fd6023b193b72f6db5626aa3225a91e
Sumppy <kristof.kelle@gmail.com>
2021-04-06 13:18:45 +0200

    defined create_board function

--- engine.py
+++ engine.py
@@ -1,3 +1,5 @@
+import random
+
 def create_board(width, height):
     '''
     Creates a new game board based on input parameters.
@@ -9,7 +11,23 @@ def create_board(width, height):
     Returns:
     list: Game board
     '''
-    pass
+    board = []
+    objects = [" ", "🌳"]
+    wall = []
+    for num in range(width):
+        wall.append(objects[1])
+    board.append(wall.copy())
+    for num in range(width-2):
+        row = []
+        for index in range(height):
+            if index == 0 or index == height-1:
+                row.append(objects[1])
+            else:
+                row.append(objects[0])
+        board.append(row.copy())
+    board.append(wall.copy())
+    return board
+
 
 
 def put_player_on_board(board, player):
