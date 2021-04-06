26f0e4e05fe9e5cb0c16efa0055819dcf4d2997e
Sumppy <kristof.kelle@gmail.com>
2021-04-06 19:34:05 +0200

    create obstacles randomly in boards

--- engine.py
+++ engine.py
@@ -14,14 +14,14 @@ def create_board(width, height):
     set_of_levels = []
     gate = []
     index_of_objects = 0
-    objects = ["🟩", "🏠", "🚪", "🟫", "🌳", "🚪", "⬛", "🔥", "🚪" ]
+    objects = ["🟩", "🏠", "🚪", "🟫", "🌳", "🚪", "⬛", "🌋", "🚪" ]
     for number in range(3):
         wall = []
         board = []
         for _ in range(width):
             wall.append(objects[index_of_objects + 1])
         board.append(wall.copy())
-        gate.append(random.randint(1, height-2))
+        gate.append(random.randint(1, height-3))
         for column in range(height-2):
             row = []
             for index in range(width):
@@ -39,12 +39,44 @@ def create_board(width, height):
         index_of_objects += 3
     return set_of_levels
 
+
+def create_obstacles_in_boards(set_of_boards, width=30, heigth=20):
+    obstacles = ["🌻", "🍄", "🔥"]
+    directions = ["h", "v"]
+    for number in range(len(set_of_boards)):
+        numbers_of_obstacles = (width*heigth)//5
+        while numbers_of_obstacles > 0:
+            obstacle_coordinate_x = random.randint(1, width-2)
+            obstacle_coordinate_y = random.randint(1, heigth-2)
+            direction = random.choice(directions)
+            if direction == "h":
+                for count in range(3):
+                    if obstacle_coordinate_x in range(width-4, width-1):
+                        set_of_boards[number][obstacle_coordinate_y][obstacle_coordinate_x - count] = obstacles[number]
+                        numbers_of_obstacles -= 1
+                    else:
+                        set_of_boards[number][obstacle_coordinate_y][obstacle_coordinate_x + count] = obstacles[number]
+                        numbers_of_obstacles -= 1
+            if direction == "v":
+                for count in range(3):
+                    if obstacle_coordinate_y in range(heigth-4, heigth - 1):
+                        set_of_boards[number][obstacle_coordinate_y - count][obstacle_coordinate_x] = obstacles[number]
+                        numbers_of_obstacles -= 1
+                    else:
+                        set_of_boards[number][obstacle_coordinate_y + count][obstacle_coordinate_x] = obstacles[number]
+                        numbers_of_obstacles -= 1
+    return set_of_boards
+
+
+                        
+
+
 def display_board(boards):
     for board in boards:
         for row in board:
             print("".join(row))
 
-display_board(create_board(30, 20))
+display_board(create_obstacles_in_boards(create_board(30, 20)))
 
 def put_player_on_board(board, player):
     '''
