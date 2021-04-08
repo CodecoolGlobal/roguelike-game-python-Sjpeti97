ef89b405ce8032205d0eee009d68622038a4ca10
balintvitai <balint.vitai@gmail.com>
2021-04-08 12:05:46 +0200

    story_mode_added_WIP

---
+++ rouge_like_storymode.py
@@ -0,0 +1,68 @@
+import time
+import shutil
+import operator
+
+
+def story():
+    main_picture = """
+
+██       ██████  ██████  ██████           ██████  ███████         ████████ ██   ██ ███████         ██████   █████  
+██      ██    ██ ██   ██ ██   ██         ██    ██ ██                 ██    ██   ██ ██              ██   ██ ██   ██ 
+██      ██    ██ ██████  ██   ██         ██    ██ █████              ██    ███████ █████           ██████  ███████ 
+██      ██    ██ ██   ██ ██   ██         ██    ██ ██                 ██    ██   ██ ██              ██      ██   ██ 
+███████  ██████  ██   ██ ██████           ██████  ██                 ██    ██   ██ ███████         ██      ██   ██                                                                                                                                                                                                 
+"""
+    text_line(main_picture.center(100))
+    text = "!Welcome to our Lord Of The Pa's game!"
+    story_line(text.center(100))
+    text2 = "* You can choose of 3 characters with different inventories and abilities."
+    print()
+    story_line(text2.center(100))
+    text3 = "* Find the rings, to move on maps and kill the final boss!"
+    print()
+    story_line(text3.center(100))
+    text4 = "* You can restore you healt and put on armor if need."
+    time.sleep(1)
+    print()
+    story_line(text4.center(100))
+    #util.clear_screen()
+
+    print("""
+██ ██     ██  █████  ██████  ███    ██ ██ ███    ██  ██████  ██ 
+██ ██     ██ ██   ██ ██   ██ ████   ██ ██ ████   ██ ██       ██ 
+██ ██  █  ██ ███████ ██████  ██ ██  ██ ██ ██ ██  ██ ██   ███ ██ 
+   ██ ███ ██ ██   ██ ██   ██ ██  ██ ██ ██ ██  ██ ██ ██    ██    
+██  ███ ███  ██   ██ ██   ██ ██   ████ ██ ██   ████  ██████  ██                                               
+""")
+
+    time.sleep(2)
+    #util.clear_screen()
+    print()
+    text6 = "* You get hit by the enemies without your weapon!"
+    story_line(text6)
+    #util.clear_screen()
+
+    luck = """
+ ██████   ██████   ██████  ██████      ██      ██    ██  ██████ ██   ██ ██ 
+██       ██    ██ ██    ██ ██   ██     ██      ██    ██ ██      ██  ██  ██ 
+██   ███ ██    ██ ██    ██ ██   ██     ██      ██    ██ ██      █████   ██ 
+██    ██ ██    ██ ██    ██ ██   ██     ██      ██    ██ ██      ██  ██     
+ ██████   ██████   ██████  ██████      ███████  ██████   ██████ ██   ██ ██                                                                      
+"""
+    text_line(luck)
+    time.sleep(2)
+    #util.clear_screen()
+
+def story_line(text):
+    for char in text:
+        print(char, sep=' ', end='', flush=True)
+        time.sleep(0.05)
+    print()    
+
+def text_line(text):
+    for char in text:
+        print(char, sep=' ', end='', flush=True)
+        time.sleep(0.005)
+    print() 
+
+story()
